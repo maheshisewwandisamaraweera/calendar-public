21a66da12fff25e5883b94a5b177c2d972ebe36b
maheshisewwandisamaraweera <maheshisewwandisamaraweera@gmail.com>
2024-03-12 11:30:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Dlogin from './Dlogin';
+import React from 'react';
+import{Routes,Route,BrowserRouter} from "react-router-dom";
+import Calendar from "./components/Calendar";
 
-function App() {
+
+
+
+ export default function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" exact element={<Dlogin/>}></Route>
+      <Route path="/calendar" exact element={<Calendar/>}></Route>
+      
+    </Routes>
+    </BrowserRouter>
+
+    </>
+    
+    
+  )
 }
 
-export default App;
